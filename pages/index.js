02c922a5eb735eac9a6e89fd3e6b988ba2c4cd09
gabrielbudke <gabrielbudke@gmail.com>
2025-12-09 20:12:34 -0300

    :construction: Alterando mensagem da página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Hello Word!</h1>
+    return <h1>Camille, eu te amo! Meu amor por você é tão extenso que até na internet você consegue encontrar por ele.</h1>
 }
 
 export default Home;
