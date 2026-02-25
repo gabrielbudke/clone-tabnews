3dd9422f6d9f5b7c5a64035ae01c3518a941d914
Gabriel Budke de Sousa <38558555+gabrielbudke@users.noreply.github.com>
2026-02-25 18:49:18 +0000

    :hammer: adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Camille, eu te amo! Meu amor por você é tão extenso que até na internet você encontra por ele.</h1>
+  return (
+    <h1>
+      Camille, eu te amo! Meu amor por você é tão extenso que até na internet
+      você encontra por ele.
+    </h1>
+  );
 }
 
 export default Home;
